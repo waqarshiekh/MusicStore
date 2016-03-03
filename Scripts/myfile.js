80ec4e2830c18de26b273b01f4be8b14c974dd33
waqarshiekh <bitf12m057@pucit.edu.pk>
2016-03-03 10:51:17 -0800

    my comment for testing

---
+++ Scripts/myfile.js
@@ -0,0 +1,22 @@
+﻿
+$(document).ready(function () {
+
+    $("#logout").hide();
+    $("#upload").hide();
+
+    
+        $("#login").click(function () {
+            $("#logout").show();
+            $("#upload").show();
+            $("#login").hide();
+        });
+    
+
+
+        $("#logout").click(function () {
+            $("#logout").hide();
+            $("#upload").hide();
+            $("#logoin").show();
+        });
+    
+});
